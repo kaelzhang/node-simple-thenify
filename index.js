73c429377e893c4b459388ddfa6d2ad99716b85a
Kael Zhang <i@kael.me>
2017-01-13 18:53:35 +0800

    main

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib')
+module.exports = function (fn) {
+  return function () {
+    const result = fn.apply(this, arguments)
+    return result && typeof result.then === 'function'
+      ? result
+      : Promise.resolve(result)
+  }
+}
--- test/simple-thenify.js
+++ test/simple-thenify.js
@@ -1,5 +1,52 @@
 const test = require('ava')
+const thenify = require('..')
 
-test('description', t => {
-  t.is(true, true)
+;[
+{
+  d: 'normal function',
+  fn: () => {
+    return 1
+  },
+  e: 1
+},
+{
+  d: 'returns promise',
+  fn: () => {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve(1)
+      }, 10)
+    })
+  },
+  e: 1
+},
+{
+  d: 'with args',
+  fn: (a, b) => {
+    return a + b
+  },
+  args: [1, 2],
+  e: 3
+},
+{
+  d: 'this',
+  fn () {
+    return this.a
+  },
+  self: {
+    a: 1
+  },
+  e: 1
+}
+].forEach(({d, fn ,e, args = [], self = null}) => {
+  test.cb(d, t => {
+    fn = thenify(fn)
+    fn.apply(self, args)
+    .then((value) => {
+      t.is(value, e)
+      t.end()
+    })
+  })
 })
+
+
