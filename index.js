1c49914173f7ff7c1487a1ddf66682c8c5ec8da6
Kael Zhang <i+github@kael.me>
2017-05-17 21:39:50 +0800

    1.0.2: really silly of me

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 module.exports = function (fn) {
   return function () {
-    const result = fn.apply(this, arguments)
-    return result && typeof result.then === 'function'
-      ? result
-      : Promise.resolve(result)
+    return Promise.resolve(fn.apply(this, arguments))
   }
 }
